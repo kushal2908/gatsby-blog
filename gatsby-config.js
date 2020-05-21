4c863e2114e3e9d540f809b9484e3f223c693a2c
Sarder Safa Bin Salam <ssafabin@gmail.com>
2020-05-22 00:54:29 +0600

    gatsby-plugin-mdx && gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,5 +22,29 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+
+    /*Creates ImageSharp nodes from image types that are supported by the Sharp image processing library and provides fields in their GraphQL types for
+      processing your images in a variety of ways including resizing, cropping, and creating responsive images */
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+
+    ////
+
+    /////////////////////////////////////////////////////
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    ////////////////////////////////////
   ],
 }
