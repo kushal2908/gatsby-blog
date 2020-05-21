c159e967dcf41907247fb2b49f09e66c20379b57
Sarder Safa Bin Salam <ssafabin@gmail.com>
2020-05-21 23:24:10 +0600

    file-system-added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,26 @@
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      option: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      option: {
+        name: `posts`,
+        path: `${__dirname}/src/post`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      option: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 }
